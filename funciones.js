ef31cad808a180e2bce1fafb408189b0d1feb28f
Oni <0nibussines@gmail.com>
2023-02-17 11:35:51 -0400

    funciones

---
+++ funciones.js
@@ -0,0 +1,28 @@
+// estas son las distintas formas de declarar una funcion
+ 
+
+
+
+//function sencilla sin variaable
+function sumaDos(a) {
+    console.log(a + 2);
+    
+}
+
+sumaDos (5);
+
+
+//con variable
+var sumaTres = function (b) {
+    console.log(b + 3)
+};
+sumaTres (5);
+
+
+
+// function de flecha 
+var sumaCuatro = (c) => {
+    console.log(c + 4)
+};
+
+sumaCuatro (5);
