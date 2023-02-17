d0e95d53288990834f893f86dc54dd5e31ed4f44
Oni <0nibussines@gmail.com>
2023-02-16 20:11:01 -0400

    Actualizacion 4

--- aprendiendo-js.js
+++ aprendiendo-js.js
@@ -1,1 +1,29 @@
 // este repositorio es para aprender a programar en JavaScript con el curso de Henry
+
+//el valor de estas variables es texto (string)
+var caja1 = "poloche amarrillo";
+
+//el valor de estas variables es Numero. el formato es asi:
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.5;
+
+//el valor de estas variables es Booleanos. el formato es asi:
+
+var estoyEntendiendo = true;
+
+var noEstoyEntendiendo = false;
+
+//el valor de esta variable es Undefined. el formato es asi:
+
+var caja1;
+
+console.log(caja1)
+
+//el valor de esta variable es Null. el formato es asi:
+
+var sinNada = null;
+// el valor de esta variable se indica cuando el programador es el que quiere definir que la variable es nula. Si no defines con el tipo de dato Null entonces seria una vareable Undefined automaticamente.
+
